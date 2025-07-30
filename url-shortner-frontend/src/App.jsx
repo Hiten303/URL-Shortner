1eecac430efa38af21b79c769b39865b62f8aa55
Hiten <hitenpaliwal097@gmail.com>
2025-07-30 19:40:31 +0530

    first commit

---
+++ url-shortner-frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
